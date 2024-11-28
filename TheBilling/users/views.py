260d1435ec803201614261afd8e9895f5f2ca5eb
Silver <mzilberman40@gmail.com>
2024-11-29 01:28:01 +0300

    The minor but very important error corrected in UserModel

--- TheBilling/users/views.py
+++ TheBilling/users/views.py
@@ -18,11 +18,11 @@ class SignInView(generic.FormView):
     """
     template_name = "users/sign_in.html"
     form_class = AuthForm
-    success_url_name = '/'
+    success_url = '/'
 
     def form_valid(self, form):
         login(self.request, form.get_user())
-        return HttpResponseRedirect(self.get_success_url_name())
+        return HttpResponseRedirect(self.get_success_url())
 
 
 def sign_out(request):
